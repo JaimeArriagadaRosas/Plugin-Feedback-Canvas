dd78ba46be02db601540606c9979835059547f44
Jaime Andrés Arriagada Rosas <163452202+JaimeArriagadaRosas@users.noreply.github.com>
2026-07-31 22:58:25 -0400

    Parche

--- vitest.config.js
+++ vitest.config.js
@@ -1,7 +1,8 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, configDefaults } from 'vitest/config';
 
 export default defineConfig({
   test: {
+    exclude: [...configDefaults.exclude, '**/tests/e2e/**'],
     passWithNoTests: true,
     fileParallelism: false,
     hookTimeout: 30000,
